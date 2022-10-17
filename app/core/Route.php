db8f8fa2ead2d7d30543f3f1d53c677a1f88e54a
vovaphp <sahno023@gmail.com>
2022-10-17 20:11:22 +0300

    Route.php

---
+++ app/core/Route.php
@@ -0,0 +1,70 @@
+<?php
+
+
+namespace core;
+
+
+class Route
+{
+    static public function init()
+    {
+        $path = $_SERVER['REQUEST_URI'];
+        $urlComponents = explode('/', $path);
+        array_shift($urlComponents);
+        if (count($urlComponents) > 3) {
+            self::notFound();
+        }
+        $controllerName = 'index';
+        if (!empty($urlComponents[0])) {
+            $controllerName = strtolower($urlComponents[0]);
+        }
+        $actionName = 'index';
+        if (!empty($urlComponents[1])) {
+            $actionName = strtolower($urlComponents[1]);
+        }
+        $controllerClass = 'controllers\\' . ucfirst($controllerName);
+        if (!class_exists($controllerClass)) {
+            self::notFound();
+        }
+        $controller = new $controllerClass;
+        if (!empty($urlComponents[2])) {
+            $actionOptions = $urlComponents[2];
+            self::actionCaller($controller,$actionName,$actionOptions);
+        }else{
+            self::actionCaller($controller,$actionName);
+        }
+
+    }
+
+    static private function actionCaller(controllerInterface $controller, string $action,string $actionOptions=null){
+        if (!method_exists($controller, $action)) {
+            self::notFound();
+        }
+        $controller->$action($actionOptions);
+    }
+
+    static public function notFound()
+    {
+        http_response_code(404);
+        exit();
+    }
+
+    static public function url(string $controller = null, string $action = null,string $actionOption = null)
+    {
+        $url = '/';
+        if (!empty($controller)) {
+            $url .= strtolower($controller);
+            if (!empty($action)) {
+                $url .= '/' . strtolower($action);
+                if (!empty($actionOption)) {
+                    $url .= '/' . $actionOption;
+                }
+            }
+        }
+        return $url;
+    }
+    static public function redirect(string $url = ''){
+        header('Location: '.$url);
+        exit();
+    }
+}
