3049984a7238025ec2745fbcb24f32c1cc86ea73
Denzik <denzik.kornilov@gmail.com>
2022-10-18 22:36:26 +0300

    JS validation of registration form without output errors(WIP)

---
+++ js/validation_script.js
@@ -0,0 +1,69 @@
+let form = document.querySelector('form');
+let formInputs = document.querySelectorAll(".input");
+let inputLogin = document.querySelector('input[name = login]')
+let inputPass = document.querySelector('input[name = password]');
+let inputPassRepeat = document.querySelector('input[name = passRepeat]');
+let inputEmail = document.querySelector('input[name = e-mail]');
+let msgError;
+
+function validEmail(email){
+    let symbolsOfEmail = email.toLowerCase().split('');
+    function isEmail (el, index, arr){
+        if (index === 0 || index === arr.length-1){
+            return false
+        }
+        return arr[index] === '@';
+    }
+    return symbolsOfEmail.findIndex(isEmail) !== -1;
+}
+
+function validPassword (password){
+    let symbolsOfPass = password.toLowerCase().split('');
+    function isNumSymbol (el, index, arr){
+        return arr[index] >= '0' && arr[index] <= '9';
+    }
+    function isLatinSymbol(el, index, arr) {
+        return arr[index] >= 'a' && arr[index] <= 'z';
+    }
+    return symbolsOfPass.findIndex(isLatinSymbol) !== -1 && symbolsOfPass.findIndex(isNumSymbol) !== -1;
+}
+
+form.onsubmit = function (e) {
+    let inputsEmpty = Array.from(formInputs).filter(input => input.value === '');
+    msgError = [];
+    formInputs.forEach(function (input) {
+        if (input.value === '') {
+            input.classList.add('error');
+        } else {
+            input.classList.remove('error');
+        }
+    });
+    if (inputsEmpty.length !== 0) {
+        msgError.push('Вы не заполнили все поля ввода.');
+        // TODO create window with msgError
+        return false;
+    } else {
+        let counter = 0;
+        if (inputLogin.length < 4 && inputLogin > 50) {
+            msgError.push('Недопустимая длинна логина.');
+            counter++;
+        }
+        if (inputPass.value !== inputPassRepeat.value) {
+            msgError.push('Пароли не совпадают.');
+            counter++;
+        } else {
+            if (validPassword(inputPass.value) !== true) {
+                counter++;
+                msgError.push('Пароль должен содержать символы латиницы и цифры.');
+            }
+        }
+        if(validEmail(inputEmail.value) !== true){
+            counter++;
+            msgError.push('Веддённый e-mail адрес не является корректным.');
+        }
+        if (counter >0){
+            // TODO create window with msgError
+            return false;
+        }
+    }
+}
