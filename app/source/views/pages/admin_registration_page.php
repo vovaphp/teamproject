2a82bab5e181602d63f78cd48baf7ddb487b285d
Denzik <denzik.kornilov@gmail.com>
2022-10-18 23:21:18 +0300

    registration page

---
+++ app/source/views/pages/admin_registration_page.php
@@ -0,0 +1,8 @@
+<h3>Registration</h3>
+<form action="<?=  \core\Route::url('admin', 'saveUser')?>" method="post">
+    <input type="text" name="login" required placeholder="login">
+    <input type="text" name="e-mail" required placeholder="e-mail">
+    <input type="text" name="password" required placeholder="password">
+    <input type="text" name="passRepeat" required placeholder="passRepeat">
+    <input type="submit" value="Create account">
+</form>
