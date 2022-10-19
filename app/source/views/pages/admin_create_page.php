35493d6b30b64e261dd45d9ce14318e9ed4623e5
Evgenij Frolov <evgenij.frolov@privatbank.ua>
2022-10-19 17:18:20 +0300

    admin_create_page.php

--- app/source/views/pages/admin_create_page.php
+++ app/source/views/pages/admin_create_page.php
@@ -6,6 +6,10 @@
     <label for="text">Text:</label>
     <textarea name="text" id="text"  rows="10" placeholder="Введите сообщение"> </textarea>
 
+    <label for="file">Добавить изображение:</label>
+    <input type="file" name="imageFile" id="file" accept="image/*">
+
+    
     <div>
         <input class="submit" type="submit" value="Create">
     </div>
