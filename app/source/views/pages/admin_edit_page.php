b1480e39a7a48eb8be4e8cd45f679c4df78613ff
Evgenij Frolov <evgenij.frolov@privatbank.ua>
2022-10-19 22:25:19 +0300

    edit admin_edit_page.php

--- app/source/views/pages/admin_edit_page.php
+++ app/source/views/pages/admin_edit_page.php
@@ -1,5 +1,5 @@
 <h2>Edit article</h2>
-<form class="create-edit-form" action="<?= \core\Route::url('admin', 'editarticle') ?>" method="post">
+<form class="create-edit-form" action="<?= \core\Route::url('admin', 'editarticle') ?>" method="post" enctype="multipart/form-data">
     <?php if (!empty($options)): ?>
 
         <label for="title">Title:</label>
@@ -8,10 +8,15 @@
         <label for="text">Text:</label>
         <textarea name="text" id="text" rows="15"><?= $text ?></textarea>
 
-        <label for="file">Добавить изображение:</label>
+        <div>
+            <img height="100px" src="<?= $imagePath ?>" alt="image">
+        </div>
+
+        <label for="file">Заменить изображение (необязательно):</label>
         <input type="file" name="imageFile" id="file" accept="image/*">
 
         <input type="hidden" name="id" value="<?=$id?>">
+        <input type="hidden" name="newImageFile" value="<?= $imagePath?>">
 
         <div>
             <input class="submit" type="submit" value="Save">
