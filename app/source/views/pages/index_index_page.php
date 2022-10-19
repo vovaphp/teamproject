a70d20b0ae7dbc4267679abc26b56195ff520c02
vovaphp <sahno023@gmail.com>
2022-10-19 15:19:07 +0300

    correct index_index_page,php

--- app/source/views/pages/index_index_page.php
+++ app/source/views/pages/index_index_page.php
@@ -1,8 +1,4 @@
-<h2>All articles</h2>
-        <?php if(!empty($articles)):?>
-            <?php foreach ($articles as $article):?>
-                <?=$article['id']?>
-            <a href="<?=\core\Route::url('index','read',$article['id'])?>"><?=$article['title']?></a>
-    - <?=$article['text']?><br/>
-            <?php endforeach;?>
-        <?php endif;?>
+<h2>Main Page</h2>
+<div id="main-side">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc congue ut erat vel malesuada. Ut scelerisque sapien in mattis aliquam. Vivamus at mollis sapien. Sed rutrum mauris et nisl molestie convallis. Nullam sit amet nisl dui. Sed id nibh quis felis imperdiet ultricies a in arcu. Duis ac velit neque. Suspendisse consequat quam posuere quam interdum, in posuere nisi ultrices. Quisque non mi nec ligula interdum auctor eget quis quam. Duis laoreet cursus ex, vitae viverra diam rutrum eu. Nam orci nunc, efficitur in sodales quis, lobortis sed orci. Phasellus eget nisl eget leo pharetra tristique sed in nulla. Nunc iaculis dolor libero, ut tincidunt massa bibendum sagittis.
+</div>
