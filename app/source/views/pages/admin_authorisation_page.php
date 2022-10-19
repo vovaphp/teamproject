872689ca83f16bade5b91fe1e4d3c18f0be1bc05
Denzik <denzik.kornilov@gmail.com>
2022-10-19 13:52:54 +0300

    fixed action

--- app/source/views/pages/admin_authorisation_page.php
+++ app/source/views/pages/admin_authorisation_page.php
@@ -1,5 +1,5 @@
 <h3>Authorisation</h3>
-<form action="<?= \core\Route::url('admin', 'authorisation') ?>" method="post">
+<form action="<?= \core\Route::url('admin', 'signIn') ?>" method="post">
     <input type="text" name="login" required placeholder="login">
     <input type="text" name="password" required placeholder="password">
     <input type="submit" value="Sign-in">
