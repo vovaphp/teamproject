9496da1ef17f2e54e8e21aa22fb2c73da2de6b74
vovaphp <sahno023@gmail.com>
2022-10-19 15:18:35 +0300

    correct adm templ

--- app/source/views/templates/admin_template.php
+++ app/source/views/templates/admin_template.php
@@ -1,17 +1,30 @@
-<!doctype html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport"
-              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Article store Dashboard</title>
-        <link rel="stylesheet" href="/css/style.css">
-    </head>
-    <body>
-    <header>
-        <h1></h1>
-    </header>
-
-    </body>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Index Page</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+<header>
+    <h1>TEAMPROJECT </h1>
+    <p>ADMIN PANEL</p>
+    <div>
+        <ul id="main-menu">
+                    <li class="page"><a href="<?=\core\Route::url('admin','createArticle')?>">Добавить статью</a></li>
+        </ul>
+    </div>
+    <div class="lang">
+        <p>Привет, <?=$_SESSION['login']?></p>
+        <p><a href="<?=\core\Route::url('admin','createUser')?>">Добавить пользователя</a></p>
+        <p><a href="<?=\core\Route::url('admin','exitUser')?>">Выйти</a></p>
+    </div>
+</header>
+<main>
+    <?php include_once \core\View::VIEWS_DIR . '/pages/'.$page.'_page.php'?>
+</main>
+<footer>
+    <p>2022@ All rights reserved</p>
+</footer>
+</body>
 </html>
