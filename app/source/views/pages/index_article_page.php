ea9f92fa1218a4e732e987d6d1e19339b4b7e1b2
vovaphp <sahno023@gmail.com>
2022-10-19 15:18:57 +0300

    correct index_article_page,php

--- app/source/views/pages/index_article_page.php
+++ app/source/views/pages/index_article_page.php
@@ -1,4 +1,10 @@
-<h2><?=$title?></h2>
-<p><?=$text ?></p>
+<div class="article">
+    <h2><?=$title?></h2>
+    <img width="50%" src="/images/articles/<?=$image?>">
+    <p><?=$text ?></p>
+    <p class="public">Опубликовано <?=$date?></p>
+</div>
+
+
 <p><a href="/">На Главную</a></p>
 
