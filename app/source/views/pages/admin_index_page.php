3f304fffefbc4830656acb1dc0bd4997043f7176
Evgenij Frolov <evgenij.frolov@privatbank.ua>
2022-10-19 16:59:30 +0300

    admin_index_page.php

---
+++ app/source/views/pages/admin_index_page.php
@@ -0,0 +1,39 @@
+<table id="articles-table">
+    <thead>
+    <tr>
+        <th>#</th>
+        <th>Title</th>
+        <th>Text</th>
+        <th>Date</th>
+        <th>Author</th>
+    </tr>
+    </thead>
+    <tbody>
+    <?php if (!empty($articles)): ?>
+        <?php foreach ($articles as $article): ?>
+            <tr>
+                <td><?= $article['id'] ?></td>
+                <td><?= $article['title'] ?></td>
+                <td><?= $article['text'] ?></td>
+                <td><?= $article['date'] ?></td>
+                <td><?= $article['login'] ?></td>
+                <td>
+                    <div class="del-edit-btn-box">
+                        <form action="<?=\core\Route::url('admin','destroy')?>" method="post">
+                            <input type="hidden" name="id" value="<?=$article['id']?>">
+                            <button><img class="icons" src="/img/delete_icon.png" alt="delete"></button>
+                        </form>
+                        <form action="<?=\core\Route::url('admin','edit')?>" method="post">
+                            <input type="hidden" name="id" value="<?=$article['id']?>">
+                            <button><img class="icons" src="/img/edit_icon.png" alt="edit"></button>
+                        </form>
+                    </div>
+
+
+
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    <?php endif; ?>
+    </tbody>
+</table>
