e290d9bb928a4a5fda61f125ebfe6b6e88e9c881
Denzik <denzik.kornilov@gmail.com>
2022-10-20 13:13:11 +0300

    fixed names of inputs

--- app/source/views/pages/admin_editUser_page.php
+++ app/source/views/pages/admin_editUser_page.php
@@ -1,9 +1,9 @@
 <h2>Edit user</h2>
-<form method="post" action="<?=core\Route::url('admin', 'editSave') ?>">
+<form method="post" action="<?=core\Route::url('admin', 'editUserSave') ?>">
     <label for="title">Login</label>
-    <input type="text" name="title" required id="title" value="<?= $user['login']?>">
+    <input type="text" name="login" required id="login" value="<?= $user['login']?>">
     <label for="text">E-mail</label>
-    <input type="text" name="title" required id="title" value="<?= $user['e-mail']?>">
+    <input type="text" name="e-mail" required id="e-mail" value="<?= $user['e-mail']?>">
     <input type="hidden" name="id" id="id" value="<?= $user['id']?>">
     <input type="submit" value="Save"/>
 </form>
