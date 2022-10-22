a2927f41ce7f08f6ad683c8f0c9f389b4d4ad577
Denzik <denzik.kornilov@gmail.com>
2022-10-22 20:14:04 +0300

    Add more info about user in method setUserSession

--- app/helpers/Session.php
+++ app/helpers/Session.php
@@ -7,9 +7,12 @@ class Session
     static public function start(){
         session_start();
     }
-     static public function setUserSession($login){
-        self::start();
-        $_SESSION['login'] = $login['login'];
+     static public function setUserSession(array $user){
+        if (!isset($_SESSION['login'])){
+            self::start();
+        }
+        $_SESSION['login'] = $user['login'];
+        $_SESSION['user_id'] = $user['id'];
     }
     public static function delUserSession(){
         session_destroy();
