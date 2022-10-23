97c98efbee57f0460143c0cc4ff868e2eac6530e
Denzik <denzik.kornilov@gmail.com>
2022-10-23 21:50:33 +0300

    add class userTable

--- app/source/views/pages/admin_users_page.php
+++ app/source/views/pages/admin_users_page.php
@@ -4,37 +4,36 @@
     <?php echo $_SESSION['error'];
     unset($_SESSION['error']); ?>
 </div>
-<?php endif;?>
-<table class="userForm">
+<table class="userTable">
     <thead>
-    <tr>
-        <th>#</th>
-        <th>Login</th>
-        <th>E-mail</th>
-        <th colspan="2">Action</th>
-    </tr>
+        <tr>
+            <th>#</th>
+            <th>Login</th>
+            <th>E-mail</th>
+            <th colspan="2">Action</th>
+        </tr>
     </thead>
     <tbody>
-    <?php if (!empty($users)): ?>
-        <?php foreach ($users as $user): ?>
-            <tr>
-                <td><?=$user['id'] ?></td>
-                <td><?=$user['login'] ?></td>
-                <td><?=$user['e-mail'] ?></td>
-                <td class="action">
-                    <form  action="<?= \core\Route::url('admin', 'deleteUser') ?>" method="post">
-                        <input type="hidden" name="id" value="<?= $user['id']?>">
-                        <button>&#128465;</button>
-                    </form>
-                </td>
-                <td class="action">
-                    <form action="<?= \core\Route::url('admin', 'editUser') ?>" method="post">
-                        <input type="hidden" name="id" value="<?= $user['id']?>">
-                        <button>&#9998;</button>
-                    </form>
-                </td>
-            </tr>
-        <?php endforeach;?>
-    <?php endif;?>
+        <?php if (!empty($users)): ?>
+            <?php foreach ($users as $user): ?>
+                <tr>
+                    <td><?=$user['id'] ?></td>
+                    <td><?=$user['login'] ?></td>
+                    <td><?=$user['e-mail'] ?></td>
+                    <td class="action">
+                        <form  action="<?= \core\Route::url('admin', 'deleteUser') ?>" method="post">
+                            <input type="hidden" name="id" value="<?= $user['id']?>">
+                            <button>&#128465;</button>
+                        </form>
+                    </td>
+                    <td class="action">
+                        <form action="<?= \core\Route::url('admin', 'editUser') ?>" method="post">
+                            <input type="hidden" name="id" value="<?= $user['id']?>">
+                            <button>&#9998;</button>
+                        </form>
+                    </td>
+                </tr>
+            <?php endforeach;?>
+        <?php endif;?>
     </tbody>
 </table>
