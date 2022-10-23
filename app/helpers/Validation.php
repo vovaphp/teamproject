dd3b0479cb3e3d9f094a3a5d421568e89b17533b
Denzik <denzik.kornilov@gmail.com>
2022-10-23 23:34:27 +0300

    add phpdoc

--- app/helpers/Validation.php
+++ app/helpers/Validation.php
@@ -4,6 +4,11 @@ namespace helpers;
 
 class Validation
 {
+    /**
+     * checked params on valid
+     * @param array $user
+     * @return bool
+     */
     static public function fieldsUser(array $user):bool{
         if (strlen($user['login']) >= 4  && $user['password'] === $user['passRepeat'] && isset($user['e-mail'])){
             return true;
