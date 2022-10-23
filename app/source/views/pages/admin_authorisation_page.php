1b9ea0ca610ac5049aa3119057a9c28560dfe23f
Denzik <denzik.kornilov@gmail.com>
2022-10-23 21:53:27 +0300

    reformat html of authorisation

--- app/source/views/pages/admin_authorisation_page.php
+++ app/source/views/pages/admin_authorisation_page.php
@@ -1,10 +1,10 @@
 <div class="userForm">
     <h2>Authorisation</h2>
-    <form action="<?= \core\Route::url('adminuser', 'signIn') ?>" method="post">
-        <label>Login</label>
-        <input type="text" name="login" required placeholder="login">
-        <label>Password</label>
-        <input type="password" name="password" required placeholder="password">
+    <form action="<?= \core\Route::url('adminusers', 'signIn') ?>" method="post">
+        <label for="login">Login</label>
+        <input type="text" name="login" id="login" required placeholder="login">
+        <label for="pass">Password</label>
+        <input type="password" name="password" id="pass" required placeholder="password">
         <input type="submit" value="Sign-in">
     </form>
 </div>
