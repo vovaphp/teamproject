b6fc4568612e434b8cb1ac429114e21b0ae6a7eb
Denzik <denzik.kornilov@gmail.com>
2022-10-23 23:21:00 +0300

    fix delUserSession

--- app/helpers/Session.php
+++ app/helpers/Session.php
@@ -29,7 +29,7 @@ class Session
      * destroy session
      */
     public static function delUserSession(){
-        $_SESSION =array();
+        self::start();
         session_destroy();
     }
 
