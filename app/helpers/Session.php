504d78bfcaebd554216a34d8c903fe05edf93be0
Denzik <denzik.kornilov@gmail.com>
2022-10-23 21:20:32 +0300

    add phpdoc

--- app/helpers/Session.php
+++ app/helpers/Session.php
@@ -4,9 +4,17 @@ namespace helpers;
 
 class Session
 {
+    /**
+     * start user session
+     */
     static public function start(){
         session_start();
     }
+
+    /**
+     * @param array $user
+     * set session with info about user
+     */
      static public function setUserSession(array $user){
         if (!isset($_SESSION['login'])){
             self::start();
@@ -14,6 +22,10 @@ class Session
         $_SESSION['login'] = $user['login'];
         $_SESSION['user_id'] = $user['id'];
     }
+
+    /**
+     * destroy session
+     */
     public static function delUserSession(){
         session_destroy();
     }
